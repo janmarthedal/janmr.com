01475a513fe9cc1bcf0242af44f384dbae2d4e69
Jan Marthedal Rasmussen <jan@janmr.com>
2016-01-10 14:58:58 +0100

    Change MathJax output to CHTML

--- _plugins/mathjax_script.rb
+++ _plugins/mathjax_script.rb
@@ -1,5 +1,5 @@
 Jekyll::Hooks.register :posts, :post_render do |post|
     if post.output.include? '<script type="math/tex'
-        post.output.sub!('<!-- nomj -->', '<script type="text/x-mathjax-config">MathJax.Hub.Config({menuSettings: {CHTMLpreview: false}});</script><script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML"></script>')
+        post.output.sub!('<!-- nomj -->', '<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML" async></script>')
     end
 end
