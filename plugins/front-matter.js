c26448f87eb403763f9e3e7593fcb031f601c9b1
Jan Marthedal Rasmussen <jan@janmr.com>
2017-06-03 08:50:41 +0200

    Tag pages (WIP)

--- plugins/front-matter.js
+++ plugins/front-matter.js
@@ -1,5 +1,3 @@
-const debug = require('debug')('front-matter');
-
 module.exports = plugin;
 
 function plugin(opts){
@@ -7,7 +5,6 @@ function plugin(opts){
     return function (files, metalsmith, done){
         setImmediate(done);
         Object.keys(files).forEach(function(file){
-            debug('front-matter working on: %s', file);
             const data = files[file];
             if (data.layout && !data.layout.match(/\.html$/))
                 data.layout = data.layout + '.html';
