1713d4e0740682583e8fafa5c43bb375411d6843
Jan Marthedal Rasmussen <jan@janmr.com>
2017-06-05 11:01:21 +0200

    Add main.js

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+ga('create','UA-46471633-1','auto');ga('send','pageview');
+(function(d) {
+  if (d.getElementById('disqus_thread')) {
+    var s = d.createElement('script')
+    s.src = 'https://janmr-blog.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    d.body.appendChild(s);
+  }
+})(document);
