deda32686bc2c3c87b4507c2568cbab8f6cec11d
Jan Marthedal Rasmussen <jan@janmr.com>
2020-04-09 08:22:49 +0200

    Add static resources

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     {
       postsRemark: allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
+        sort: { order: ASC, fields: [frontmatter___date] }
         limit: 1000
       ) {
         edges {
