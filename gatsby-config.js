4a1586344daad59885b107f672f4feeaef159b0a
Jan Marthedal Rasmussen <jan@janmr.com>
2020-04-09 13:33:52 +0200

    Add site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `janmr.com`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/rightarrow.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
