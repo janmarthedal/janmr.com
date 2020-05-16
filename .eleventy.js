8a7d80614112503bd22396865357eb2c820c1299
Jan Marthedal Rasmussen <jan@janmr.com>
2020-05-16 16:43:59 +0200

    Show post tags

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,31 @@
-function getMarkdownLib() {
-  let markdownIt = require("markdown-it");
-  let markdownItKaTeX = require("@janmarthedal/markdown-it-katex");
-  let options = {
-    html: true,
-  };
-  return markdownIt(options).use(markdownItKaTeX);
-}
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItKaTeX = require("@janmarthedal/markdown-it-katex");
 
 module.exports = function (eleventyConfig) {
-  const inputFolder = 'src';
+  const inputFolder = "src";
 
-  eleventyConfig.setLibrary("md", getMarkdownLib());
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({ html: true }).use(markdownItKaTeX)
+  );
 
   eleventyConfig.addPassthroughCopy(`${inputFolder}/lab`);
   eleventyConfig.addPassthroughCopy(`${inputFolder}/media`);
+
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+
+  eleventyConfig.addFilter("readableDate", (date) =>
+    DateTime.fromISO(date, { zone: "utc" }).toFormat("LLLL dd, yyyy")
+  );
+
+  eleventyConfig.addFilter("htmlDateString", (date) =>
+    DateTime.fromISO(date, { zone: "utc" }).toISODate()
+  );
+
+  eleventyConfig.addFilter("excludeElement", (list, element) =>
+    list.filter(item => item !== element)
+  );
 
   return {
     dir: {
