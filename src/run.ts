c7d537c0214a9c445a0377ef69c00a6e81a4989e
Jan Marthedal Rasmussen <jan@janmr.com>
2023-07-30 10:45:36 +0200

    Process njk files

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,8 @@
 import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { dirname, extname, join } from 'path';
 import { globIterateSync } from 'glob';
+import nunjucks from 'nunjucks';
+import { read as matterRead } from 'gray-matter';
 
 const SOURCE_DIR = 'content';
 const COPY_PATTERNS = ['files/**/*', 'media/**/*', 'lab/**/*.js', 'lab/**/*.js.map', 'icon-48x48.png'];
@@ -8,6 +10,24 @@ const CSS_INPUT = ['css/normalize.css', 'css/styles.less'];
 const SOURCE_PATTERN = '**/*';
 const SITE_DIR = '_site';
 const IGNORE_PATTERNS = [...COPY_PATTERNS, ...CSS_INPUT];
+const LAYOUT_DIR = 'layouts';
+
+const metadata = {
+    "title": "janmr blog",
+    "url": "https://janmr.com/blog/",
+    "author": {
+        "name": "Jan Marthedal Rasmussen",
+        "email": "jan@janmr.com"
+    },
+    "feed": {
+        "subtitle": "The blog of Jan Marthedal Rasmussen",
+        "url": "https://janmr.com/blog/feed.xml",
+        "id": "https://janmr.com/blog/"
+    }
+};
+
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(LAYOUT_DIR), { autoescape: true });
+env.addFilter('url', (path: string) => '' + path);
 
 function writeFile(filename: string, contents: string | Buffer) {
     console.log('write', filename);
@@ -23,12 +43,22 @@ for (const filename of globIterateSync(COPY_PATTERNS, { cwd: SOURCE_DIR, nodir: 
 }
 
 for (const filename of globIterateSync(SOURCE_PATTERN, { cwd: SOURCE_DIR, nodir: true, ignore: IGNORE_PATTERNS })) {
+    console.log('process', filename);
     const ext = extname(filename);
-    if (ext === '.md') {
-        console.log('markdown', filename);
-    } else if (ext === '.njk') {
-        console.log('nunjucks', filename);
-    } else {
+    if (!['.md', '.njk'].includes(ext)) {
         throw new Error(`Unsupported extension ${ext} for ${filename}`);
     }
+    if (ext === '.md') {
+        // ignore for now
+        continue;
+    }
+    const { data, content } = matterRead(join(SOURCE_DIR, filename));
+    let output;
+    if (data.layout) {
+        output = env.render(data.layout + '.njk', { ...data, metadata, content });
+    } else {
+        output = env.renderString(content, { ...data, metadata });
+    }
+    const outFilename = filename.slice(0, -ext.length) + '.html';
+    writeFile(outFilename, output);
 }
